2314728e6c006d8b9a92319a106ea3f0f5e80371
Mark Martines <mark-martines@hotmail.com>
2024-02-18 22:03:09 +0100

    Added carousel to footer.php and added images

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,84 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style> <?php include './assets/css/footer.css'; ?> </style>
 
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+
+<!-- Owl Stylesheets -->
+<link rel="stylesheet" href="./assets/js/OwlCarousel2-2.3.4/dist/assets/owl.carousel.min.css" />
+<link rel="stylesheet" href="./assets/js/OwlCarousel2-2.3.4/dist/assets/owl.theme.default.min.css">
+
+
+<!-- javascript -->
+<script src="./assets/js/OwlCarousel2-2.3.4/dist/owl.carousel.min.js"></script>
+
+<div class="owl-carousel">
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/aad_20leeuwe.jpg'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/Baker%20Bart%20kazerneplein.jpg'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/bartjanssenassurantien.png'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/botex%20wasserij%20logo.jpg'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/Dierx%20Groep.jpg'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/domiveranda.png'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/hetfinancieeladvieshuis.png'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/Hotel-en-Grand-Cafe-De-Pauw.png'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/janwuts.jpeg'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/meneerkes.jpg'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/mts.gif'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/sif.jpeg'>
+        </a>
+    </div>
+    <div class="sponsoren-carousel">
+        <a href='' target='blank'>
+            <img src='./assets/images/logos-sjponsoren/van%20Tuyl%20beheer.jpg'>
+        </a>
+    </div>
+</div>
+
 <footer>
     <div class="footer">
         <div class="row">
@@ -9,7 +87,31 @@
         </div>
 
         <div class="row">
-            &copy; <?php echo date("Y"); ?> V.V. de Tuinhagedisse || Test Designed By: Sjoerd & Mark 
+            &copy; <?php echo date("Y"); ?> V.V. de Tuinhagedisse
         </div>
     </div>
 </footer>
+
+<script>
+    $('.owl-carousel').owlCarousel({
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        center: false,
+        loop:true,
+        margin:50,
+        dots:false,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:4,
+            },
+            1000:{
+                items:6,
+            }
+        }
+    })
+</script>
