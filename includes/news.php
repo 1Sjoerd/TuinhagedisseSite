964e49b22345bcbbfcc56e4382275322bfad71fb
Mark Martines <mark-martines@hotmail.com>
2024-03-17 22:05:25 +0100

    Created a news slider (news.php and news.css) by using the existing OwlCarousel which is al ready in use by the slider in the footer.
    Altered the owlcarousel js in footer.php for support of two carousels. Hence why there is no JS on news.php.
    
    Altered index.php to include the news slider.
    
    Added a two pictures for testing the slider with a more reallife data set.

---
+++ includes/news.php
@@ -0,0 +1,99 @@
+<style> <?php include './assets/css/standardblock.css'; ?> </style>
+<style> <?php include './assets/css/news.css'; ?> </style>
+
+<div class="block-overview">
+    <div class="heading-title">
+        <h2 class="block-title"> Letste nuujts </h2>
+    </div>
+    <div class="news-body">
+        <div id="news-slider" class="owl-carousel news-slider">
+            <div class="news-post">
+                <div class="post-img">
+                    <img src="./assets/images/news/Poster_final_klein.jpg" alt="">
+                </div>
+                <div class="post-content">
+                    <h3 class="post-title">
+                        <a href="#">Trek 'm "Euver de sjreef"</a>
+                    </h3>
+                    <p class="post-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......</p>
+                    <span class="post-date"><i class="fa fa-clock-o"></i>17 mrt, 2024</span>
+                    <a href="#" class="read-more">Laes meer</a>
+                </div>
+            </div>
+
+            <div class="news-post">
+                <div class="post-img">
+                    <img src="./assets/images/news/Vogels.JPG" alt="">
+                </div>
+                <div class="post-content">
+                    <h3 class="post-title">
+                        <a href="#">Jules Vogels</a>
+                    </h3>
+                    <p class="post-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......</p>
+                    <span class="post-date"><i class="fa fa-clock-o"></i>5 mrt, 2024</span>
+                    <a href="#" class="read-more">Laes meer</a>
+                </div>
+            </div>
+
+            <div class="news-post">
+                <div class="post-img">
+                    <img src="./assets/images/TuinhagedisseLogo.png" alt="">
+                </div>
+                <div class="post-content">
+                    <h3 class="post-title">
+                        <a href="#">Lorem ipsum dolor sit amet.</a>
+                    </h3>
+                    <p class="post-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......</p>
+                    <span class="post-date"><i class="fa fa-clock-o"></i>Out 27, 2019</span>
+                    <a href="#" class="read-more">Laes meer</a>
+                </div>
+            </div>
+
+            <div class="news-post">
+                <div class="post-img">
+                    <img src="https://images.unsplash.com/photo-1576659531892-0f4991fca82b?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=301&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=501" alt="">
+                </div>
+                <div class="post-content">
+                    <h3 class="post-title">
+                        <a href="#">Lorem ipsum dolor sit amet.</a>
+                    </h3>
+                    <p class="post-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......</p>
+                    <span class="post-date"><i class="fa fa-clock-o"></i>Out 27, 2019</span>
+                    <a href="#" class="read-more">Laes meer</a>
+                </div>
+            </div>
+
+            <div class="news-post">
+                <div class="post-img">
+                    <img src="https://images.unsplash.com/photo-1586083702768-190ae093d34d?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=305&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=505" alt="">
+                </div>
+                <div class="post-content">
+                    <h3 class="post-title">
+                        <a href="#">Lorem ipsum dolor sit amet.</a>
+                    </h3>
+                    <p class="post-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......</p>
+                    <span class="post-date"><i class="fa fa-clock-o"></i>Out 27, 2019</span>
+                    <a href="#" class="read-more">Laes meer</a>
+                </div>
+            </div>
+
+            <div class="news-post">
+                <div class="post-img">
+                    <img src="https://images.unsplash.com/photo-1484656551321-a1161420a2a0?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=306&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=80&w=506" alt="">
+                </div>
+                <div class="post-content">
+                    <h3 class="post-title">
+                        <a href="#">Lorem ipsum dolor sit amet.</a>
+                    </h3>
+                    <p class="post-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consectetur cumque dolorum, ex incidunt ipsa laudantium necessitatibus neque quae tempora......</p>
+                    <span class="post-date"><i class="fa fa-clock-o"></i>Out 27, 2019</span>
+                    <a href="#" class="read-more">Laes meer</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+
+</script>
