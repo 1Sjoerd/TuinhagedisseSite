c795cde50004ff2741a1f749f2f654e9bdd5454c
1Sjoerd <sjburhenne@gmail.com>
2024-08-26 23:28:02 +0200

    Css changes

--- includes/prinsen.php
+++ includes/prinsen.php
@@ -1,24 +1,24 @@
-<style> <?php include './assets/css/prinsen.css'; ?> </style>
+<link rel="stylesheet" href="./assets/css/prinsen.css">
 
 <div class="block-overview">
     <div class="heading-title">
         <h2 class="block-title"> Prinsen </h2>
     </div>
     <div class="prinsen-body">
-        <div class='card'>
-            <div class='image-section'>
-                <img src="./assets/images/prinsen/jannie.jpg" alt="Prins" />
+        <div class="card">
+            <div class="image-section">
+                <img src="./assets/images/prinsen/jannie.jpg" alt="Prins Jannie I" loading="lazy" />
             </div>
-            <div class='text-section'>
+            <div class="text-section">
                 <p class="title-text">Jannie I</p>
                 <p class="subtitle-text">"Same mit mien zoon op de troon!"</p>
             </div>
         </div>
-        <div class='card'>
-            <div class='image-section'>
-                <img src="./assets/images/prinsen/joey.jpg" alt="Prins" />
+        <div class="card">
+            <div class="image-section">
+                <img src="./assets/images/prinsen/joey.jpg" alt="Prins Joey I" loading="lazy"/>
             </div>
-            <div class='text-section'>
+            <div class="text-section">
                 <p class="title-text">Joey I</p>
                 <p class="subtitle-text">"In mien eendje is ‘t echt neet gaaf maar mit zien 4e zigge veer Leeve ALAAF!"</p>
             </div>
