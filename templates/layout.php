5f8c44db5d1b6be7002fd6c663994147d8584c2b
Mark Martines <75362952+Waylikemark@users.noreply.github.com>
2024-11-14 21:23:49 +0100

    Update layout.php

--- templates/layout.php
+++ templates/layout.php
@@ -51,6 +51,32 @@
     <!-- Link naar je JavaScript-bestand(en) -->
     <script src="./assets/js/header.js"></script>
 
+    
+        <?php
+$sql = "SELECT * FROM `news` ORDER BY `date` DESC";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0 ) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<div class='modal' id='".$row["id"]."news'>";
+            echo "<div class='modal-content'>";
+                echo "<div class='block-overview'>";
+                    echo "<div class='heading-title'>";
+                        echo "<h2 class='block-title'>".$row["title"]."</h2>";
+                        echo "<a class='close-button hide-modal' id='".$row['id']."news' data-id='".$row['id']."news'>&#10006;</a>";
+                    echo "</div>";
+                    echo "<div class='block-text'>";
+                        echo $row["text"];
+                    echo "</div>";
+                echo "</div>";
+            echo "</div>";
+        echo "</div>";
+    }
+}
+?>
+
+<script src="./assets/js/modal.js"></script>
+    
     <!-- Voeg eventuele andere scripts toe -->
 </body>
 </html>
