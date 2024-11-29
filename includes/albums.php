43d5cc8c682020d7c25594db550ad7760216400b
I515684 <i515684@fontysict.nl>
2024-11-29 16:12:07 +0100

    test

--- includes/albums.php
+++ includes/albums.php
@@ -91,7 +91,7 @@
                 const albumId = albumIdMatch ? albumIdMatch[1] : null;
 
                 if (isMobile && albumId) {
-                    albumLink = `fb://albums?id=${albumId}`;
+                    albumLink = `fb://page?id=${albumId}`;
                 }
 
                 albumItem.innerHTML = `
