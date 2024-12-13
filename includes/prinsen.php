0f947428b796923a717ea7ed22ea47d25182a0fc
Mark Martines <mark-martines@hotmail.com>
2024-12-13 14:57:55 +0100

    The prinse data is now collected from the DB instead of hardcoded. Only the image is still hardcoded

--- includes/prinsen.php
+++ includes/prinsen.php
@@ -5,28 +5,47 @@
         <h2 class="block-title"> Veurgengers </h2>
     </div>
     <div class="prinsen-body">
-        <div class="card-container">
-            <div class="card">
-                <div class="image-section">
-                    <img src="./assets/images/prinsen/rick.jpg" alt="Prins Rick I" loading="lazy" />
-                    <div class="text-overlay">
-                        <p class="title-text">RICK I</p>
-                    </div>
-                </div>
-            </div>
-            <div class="card-text">"Vastelaovend raak biej os de goje toon!"</div>
-        </div>
-        <div class="card-container">
-            <div class="card">
-                <div class="image-section">
-                    <img src="./assets/images/prinsen/yara.jpg" alt="Prinses Yara I" loading="lazy" />
-                    <div class="text-overlay">
-                        <p class="title-text">YARA I</p>
-                    </div>
-                </div>
-            </div>
-            <div class="card-text">"Lach, dans, maak plezeer, de rest kump morge weer!"</div>
-        </div>
+        <?php
+            $sql = "SELECT * FROM `prinse` ORDER BY `year` DESC LIMIT 1";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0 ) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<div class='card-container'>";
+                        echo "<div class='card'>";
+                            echo "<div class='image-section'>";
+                                echo "<img src='./assets/images/prinsen/rick.jpg' alt='Prins ".$row["firstname"]." ".$row["number"]."' loading='lazy' />";
+                                echo "<div class='text-overlay'>";
+                                    echo "<p class='title-text'>".$row["firstname"]." ".$row["number"]."</p>";
+                                echo "</div>";
+                            echo "</div>";
+                        echo "</div>";
+                        echo "<div class='card-text'>\"".$row["motto"]."\"</div>";
+                    echo "</div>";
+                }
+            }
+        ?>
+
+        <?php
+            $sql = "SELECT * FROM `jeugdprinse` ORDER BY `year` DESC LIMIT 1";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0 ) {
+                while ($row = $result->fetch_assoc()) {
+                    echo "<div class='card-container'>";
+                        echo "<div class='card'>";
+                            echo "<div class='image-section'>";
+                                echo "<img src='./assets/images/prinsen/yara.jpg' alt='Prins ".$row["firstname"]." ".$row["number"]."' loading='lazy' />";
+                                echo "<div class='text-overlay'>";
+                                    echo "<p class='title-text'>".$row["firstname"]." ".$row["number"]."</p>";
+                                echo "</div>";
+                            echo "</div>";
+                        echo "</div>";
+                        echo "<div class='card-text'>\"".$row["motto"]."\"</div>";
+                    echo "</div>";
+                }
+            }
+        ?>
     </div>
 </div>
 
