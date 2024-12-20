379ef6563239da3f0c357467e3e3a5c1cec0be35
Mark Martines <75362952+Waylikemark@users.noreply.github.com>
2024-12-20 17:30:58 +0100

    Update news-template.php

--- includes/news-template.php
+++ includes/news-template.php
@@ -1,10 +1,24 @@
 <style> <?php include './assets/css/standardblock.css'; ?> </style>
+<style> <?php include './assets/css/newstemplate.css'; ?> </style>
 
 <div class="block-overview">
     <div class="heading-title">
         <h2 class="block-title"> <?= htmlspecialchars($row["title"]) ?> </h2>
     </div>
     <div class="block-text">
-        <?= htmlspecialchars($row["text"]) ?>
+        <div class="news-row">
+        <?php
+            if ($row['image_url'] != '') {
+                echo "<div class='news-column'>";
+                    echo "<img src='".htmlspecialchars($row["image_url"])."' alt='".htmlspecialchars($row["title"])."'>";
+                echo "</div>";
+                echo "<div class='news-column'>";
+                    echo htmlspecialchars($row["text"]);
+                echo "</div>";
+            } else {
+                echo htmlspecialchars($row["text"]);
+            }
+        ?>
+        </div>
     </div>
 </div>
