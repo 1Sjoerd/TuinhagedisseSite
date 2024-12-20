753acc40340492460138a6233e6a4ccbd52900b2
Mark Martines <mark-martines@hotmail.com>
2024-12-20 20:43:03 +0100

    Created an extra layout and header but without banner. This way the dynamic pages don't have the banner on them.

---
+++ includes/headerTemplate.php
@@ -0,0 +1,40 @@
+<?php require './vendor/autoload.php'; ?>
+
+<style>
+    <?php include './assets/css/header.css'; ?>
+</style>
+
+<a href="./index.php">
+    <img class="logo" src="./assets/images/TuinhagedisseLogo.png"/>
+</a>
+<header class="site-header">
+    <div class="header__content--flow">
+        <section class="header-content--left">
+            <a href="./index.php" class="brand-logo"></a>
+            <button class="nav-toggle">
+                <span class="toggle--icon"></span>
+            </button>
+        </section>
+        <section class="header-content--right">
+            <nav class="header-nav" role="navigation">
+                <ul class="nav__list" aria-expanded="false">
+                    <li class="list-item">
+                        <a class="nav__link" href="./index.php">Akteweel</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav__link" href="./vereniging.php">Vereniging</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav__link" href="./historie.php">Historie</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav__link" href="./sjponsore.php">Sjponsore</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav__link" href="./kontak.php">Kóntak</a>
+                    </li>
+                </ul>
+            </nav>
+        </section>
+    </div>
+</header>
