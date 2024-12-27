e0b9fafa85cb767009ff8d138989bbd676508417
Mark Martines <75362952+Waylikemark@users.noreply.github.com>
2024-12-27 11:40:53 +0100

    Create sendmail_test.php

---
+++ includes/mailTemplate/sendmail_test.php
@@ -0,0 +1,18 @@
+<?php
+// LOKAAL TESTEN NIET MOGELIJK! MAIL WERKT ALLEEN BIJ ONE.COM
+// Multiple recipients
+$to = $_POST['email']; // note the comma
+
+// Subject
+$subject = 'Bevestiging aanmelding'';
+
+include 'conformationEmail.php';
+
+// To send HTML mail, the Content-type header must be set
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+$headers[] = 'From: VV de Tuinhagedisse <no-reply@vvdetuinhagedisse.nl>';
+
+// Mail it
+mail($to, $subject, $message, implode("\r\n", $headers));
+?>
