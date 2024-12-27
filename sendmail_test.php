db47043dce92ceb8ef52d066c4da36b86af4b686
Mark Martines <75362952+Waylikemark@users.noreply.github.com>
2024-12-27 11:26:25 +0100

    Update sendmail_test.php
    mail to is now fetched from the form

--- sendmail_test.php
+++ sendmail_test.php
@@ -1,19 +1,16 @@
 <?php
 // LOKAAL TESTEN NIET MOGELIJK! MAIL WERKT ALLEEN BIJ ONE.COM
 // Multiple recipients
-$to = 'mark-martines@hotmail.com'; // note the comma
+$to = $_POST['email']; // note the comma
 
 // Subject
-$subject = 'Bevestiging';
+$subject = 'Bevestiging aanmelding'';
 
 include 'conformationEmail.php';
 
 // To send HTML mail, the Content-type header must be set
 $headers[] = 'MIME-Version: 1.0';
 $headers[] = 'Content-type: text/html; charset=iso-8859-1';
-
-// Additional headers
-// $headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
 $headers[] = 'From: VV de Tuinhagedisse <no-reply@vvdetuinhagedisse.nl>';
 
 // Mail it
