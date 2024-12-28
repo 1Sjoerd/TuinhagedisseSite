22f04cc78356408cacd92e623a0536d4e3d18ae6
Mark Martines <mark-martines@hotmail.com>
2024-12-28 23:57:37 +0100

    forgot to include login.php

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+// Specificeer de specifieke inhoud van deze pagina
+$content = 'pages/login.php';
+
+// Inclusie van het algemene sjabloon
+include_once 'templates/layoutNoBanner.php';
+?>
