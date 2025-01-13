41440844ff986b0328276b960a47d438f18de54d
Mark Martines <mark-martines@hotmail.com>
2025-01-14 00:04:31 +0100

    small improvement

--- includes/dashboardIncludes/add_event.php
+++ includes/dashboardIncludes/add_event.php
@@ -2,9 +2,6 @@
 session_start();
 include '../../templates/dbconnection.php';
 
-// Increase memory limit at the beginning of the script
-ini_set('memory_limit', '256M');
-
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $id = $_POST['eventId'];
     $title = $_POST['eventTitle'];
@@ -12,14 +9,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $date = $_POST['eventDate'];
     $registration_enddate = !empty($_POST['eventRegistration_enddate']) ? $_POST['eventRegistration_enddate'] : null;
 
-
     if (isset($_POST['eventRegistration_needed'])) {
         $registration_needed = "1";
-        
+
         if (isset($_POST['eventRegistration_fields']) && is_array($_POST['eventRegistration_fields'])) {
-            // Haal de waarden van de geselecteerde checkboxes op
+            // Haal de waarden van de geselecteerde opties uit de select box
             $registration_fields = $_POST['eventRegistration_fields'];
-    
+
             // Controleer of "adres" is geselecteerd en voeg de extra velden toe
             if (in_array('adres', $registration_fields)) {
                 $registration_fields = array_merge(
@@ -29,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 // Verwijder "adres" als je alleen de subvelden wilt tonen
                 $registration_fields = array_diff($registration_fields, ['adres']);
             }
-    
+
             // Combineer de waarden in een komma-gescheiden string
             $registration_fields_string = implode(', ', $registration_fields);
         } else {
@@ -42,9 +38,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $registration_fields_string = NULL;
     }
     
-    
     if ($id) {
-        // Update existing news item
+        // Update bestaand evenement
         $stmt = $conn->prepare("UPDATE events SET title = ?, location = ?, date = ?, registration_needed = ?, registration_fields = ?, registration_enddate = ? WHERE id = ?");
         $stmt->bind_param("ssssssi", $title, $location, $date, $registration_needed, $registration_fields_string, $registration_enddate, $id);
         if ($stmt->execute()) {
@@ -53,7 +48,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             error_log("Error updating event item: " . $stmt->error);
         }
     } else {
-        // Insert placeholder to get new ID
+        // Voeg nieuw evenement toe
         $stmt = $conn->prepare("INSERT INTO events (title, location, date, registration_needed, registration_fields, registration_enddate) VALUES (?, ?, ?, ?, ?, ?)");
         $stmt->bind_param("ssssss", $title, $location, $date, $registration_needed, $registration_fields_string, $registration_enddate);        
         $stmt->execute();
